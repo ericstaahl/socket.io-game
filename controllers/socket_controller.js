88fe5c22616c3ffe2bc0c31c3f2c352cc384308c
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-03-16 20:41:19 +0100

    Now adds a user to the users object when submitting the form on the page.

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -2,8 +2,19 @@
  * Socket Controller
  */
 const debug = require('debug')('game:socket_controller');
+// Object containing all users
+const users = {};
+
+const handleUserJoined = async function(username) {
+    //add the user to the users object
+    debug('Listening for "user-join"')
+    users[this.id] = username;
+    debug(users);
+};
 
 module.exports = function (socket) {
+    socket.on('user:joined', handleUserJoined);
+
     socket.on('message', (msg) => {
         debug('Listening for "message"')
         console.log('Message: ', msg);
--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,3 +1,14 @@
 const socket = io();
+const startForm = document.querySelector('#start-form');
+// const usernameFormBtn = document.querySelector('#usernameFormBtn')
+
+let username = null;
+startForm.addEventListener('submit', e => {
+    e.preventDefault();
+    username = startForm.username.value;
+    socket.emit('user:joined', username, (status) => {
+        console.log('Server has responded', status)
+    });
+});
 
 socket.emit('message', 'Hi from the client');
