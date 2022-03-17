80d755e9e79c5ea8cb59a13375138d220a5c55ab
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-03-17 11:05:32 +0100

    Added listener for sending message between two sockets

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -32,17 +32,24 @@ const handleUserJoined = async function (username, callback) {
 };
 
 const handleDisconnect = async function () {
-    //add the user to the users object
+    //remove the user from the users object
     debug('Listening for "user-disconnected"')
     console.log(`${users[this.id]} has disconnected`);
     delete users[this.id];
     debug(users);
 };
 
+// Send a message to another socket.id
+const privateConnection = async function (opponentsSocket) {
+    socket.to(opponentsSocket).emit("challenge", socket.id, "I challenge you!");
+};
+
 module.exports = function (socket) {
     socket.on('disconnect', handleDisconnect);
 
     socket.on('user:joined', handleUserJoined);
+
+    socket.on('findGame', privateConnection);
 
     socket.on('message', (msg) => {
         debug('Listening for "message"')
